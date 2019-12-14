c9331bcb07f59652975317f02fd3e96576898c8c
jaime-varela <varela.jaime01@gmail.com>
2019-12-14 12:35:30 -0500

    initial bad fcn templates

---
+++ analasisAPI/subscriptionFinder.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+
+# TODO: All of it
+#
+#
+
+# returns a numeric value between zero and one if a signal has a period
+def isPeriodic(groupedDF, fourierThreshold):
+    return 0.0
+
+
+# returns the periods of the signal based on threshold conditions
+def getPeriods(groupedDF, params):
+    return [0.0]
+
